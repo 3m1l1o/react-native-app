f43c177249a9353b0fddd06838603e91bb56e0c5
Juan Pablo Tello <juantello@MacBook-Air-de-Juan.local>
2023-08-03 22:36:02 -0300

    import fixs - counter exercise

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,30 @@
-import React,{useState} from 'react';
-import { Text, Button, StyleSheet, View} from 'react-native';
+import React, { useState } from "react";
+import { Text, Button, StyleSheet, View } from "react-native";
 
-const CounterScreen = ()=>{
-    const [ counter, setCounter]= useState(rgbRandom());
-    return(
-        <View>
-            <Button 
-            title="Increase Number"
-            onPress={()=>{
-            setCounter( counter + 1)
-            }}/>
-            <Button 
-            title="Dicrease Number"
-            onPress={()=>{
-            setCounter( counter - 1)
-            }}/>
-            <Text> Counter Numbre : { counter }</Text>
-        </ View>
-    
-)};
-const styles=  StyleSheet.create({});
+const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
+  const counterHandler = (amount) => {
+    if ((counter <= 0 && amount < 0) || (counter >= 9 && amount > 0)) return;
+
+    setCounter(counter + amount);
+  };
+
+  return (
+    <View>
+      <Button
+        title="Increase Number"
+        onPress={() => counterHandler(1)}
+      />
+      <Button
+        title="Dicrease Number"
+        onPress={() => counterHandler(-1)}
+      />
+      <Text> Counter Numbre : {counter}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
 export default CounterScreen;
