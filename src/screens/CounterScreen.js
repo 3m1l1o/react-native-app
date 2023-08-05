16f2a820c46b560fbdf83fff905285e5d0d15e5b
unknown <emiliogallo80@gmail.com>
2023-08-05 17:56:11 -0300

    Curso git

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -4,6 +4,7 @@ import { Text, Button, StyleSheet, View} from react-Native;
 import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react/cjs/react.production.min";
 
 const CounterScreen = ()=>{
+    console.log('commit')
     const [ counter, setCounter]= useState(0);
     return
         <View>
